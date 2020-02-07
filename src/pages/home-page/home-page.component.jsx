2f7e2804cc838a66b6bef3acf4548b1be512a34b
lalkovski <ilija.lalkovski@inplayer.com>
2020-02-07 15:59:04 +0100

    Add styled-components to project

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import './home-page.styles.scss';
+
+import { HomePageContainer } from './home-page.styles';
 
 import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
-	<div className="homepage">
+	<HomePageContainer>
 		<Directory />
-	</div>
+	</HomePageContainer>
 );
 
 export default HomePage;
